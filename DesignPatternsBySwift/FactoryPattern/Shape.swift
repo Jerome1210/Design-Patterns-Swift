2c7a959aa55c321047027630c9bdd21685ff65c9
Jerome1210 <b99705008@ntu.edu.tw>
2017-01-24 23:37:46 +0800

    feat: add general Shape class

---
+++ DesignPatternsBySwift/FactoryPattern/Shape.swift
@@ -0,0 +1,15 @@
+//
+//  Shape.swift
+//  DesignPatternsBySwift
+//
+//  Created by Lee on 24/01/2017.
+//
+//
+
+import Foundation
+
+class Shape {
+    func draw() -> () {
+        print("Shape's default drawing");
+    }
+}
