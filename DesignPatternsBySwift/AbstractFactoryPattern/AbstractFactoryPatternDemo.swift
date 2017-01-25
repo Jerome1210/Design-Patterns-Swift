f87fc77be3973902bd17a6fcbc7d1c25b886cd73
Jerome1210 <b99705008@ntu.edu.tw>
2017-01-26 02:28:06 +0800

    feat: add AbstractFactoryPatternDemo class

--- DesignPatternsBySwift/AbstractFactoryPattern/AbstractFactoryPatternDemo.swift
+++ DesignPatternsBySwift/AbstractFactoryPattern/AbstractFactoryPatternDemo.swift
@@ -8,6 +8,40 @@
 
 import UIKit
 
-class AbstractFactoryPatternDemo: NSObject {
+class AbstractFactoryPatternDemo: UIViewController {
+    lazy var shapeFactory = {
+        FactoryProducer().getFactory(choice: "SHAPE")
+    }()
+    lazy var shape1:Shape? = {
+        return self.shapeFactory?.getShape(shape: "Circle")
+    }()
+    lazy var shape2:Shape? = {
+        return self.shapeFactory?.getShape(shape: "Rectangle")
+    }()
+    lazy var shape3:Shape? = {
+        return self.shapeFactory?.getShape(shape: "Square")
+    }()
+    
+    lazy var colorFactory = {
+        return FactoryProducer().getFactory(choice: "COLOR")
+    }()
+    
+    lazy var color1:Color? = {
+        return self.colorFactory?.getColor(color: "Red")
+    }()
+    lazy var color2:Color? = {
+        return self.colorFactory?.getColor(color: "Green")
+    }()
+    lazy var color3:Color? = {
+        return self.colorFactory?.getColor(color: "Blue")
+    }()
+    override func viewDidLoad() {
+        shape1?.draw()
+        shape2?.draw()
+        shape3?.draw()
+        color1?.fill()
+        color2?.fill()
+        color3?.fill()
+    }
 
 }
