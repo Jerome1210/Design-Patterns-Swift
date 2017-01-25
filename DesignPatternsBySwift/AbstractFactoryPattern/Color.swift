35d74349fde8d169734d767ceaded9b08dd6f21e
Jerome1210 <b99705008@ntu.edu.tw>
2017-01-26 01:36:46 +0800

    feat: add a gerenal Color class which is designed to be subclassed

---
+++ DesignPatternsBySwift/AbstractFactoryPattern/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  DesignPatternsBySwift
+//
+//  Created by Lee on 26/01/2017.
+//
+//
+
+import Foundation
+
+class Color {
+    func fill() -> () {
+        print("default color fill")
+    }
+}
