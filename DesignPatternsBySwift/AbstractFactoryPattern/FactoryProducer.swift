9e89fa133ee9a794b38c46da45242358806c3f4f
Jerome1210 <b99705008@ntu.edu.tw>
2017-01-26 02:27:36 +0800

    feat: add FactoryProducer class

--- DesignPatternsBySwift/AbstractFactoryPattern/FactoryProducer.swift
+++ DesignPatternsBySwift/AbstractFactoryPattern/FactoryProducer.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+class FactoryProducer {
+    func getFactory(choice: String) -> AbstractFactory? {
+        if choice == "SHAPE" {
+            return ShapeFactoryTwo()
+        } else if choice == "COLOR" {
+            return ColorFactory()
+        }
+        return nil
+    }
+}
