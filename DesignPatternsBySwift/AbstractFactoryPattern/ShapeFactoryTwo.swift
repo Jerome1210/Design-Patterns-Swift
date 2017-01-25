3544a3850922205da6bed2628030423cf255b5d8
Jerome1210 <b99705008@ntu.edu.tw>
2017-01-26 02:23:54 +0800

    feat: add ShapeFactoryTwo class

--- DesignPatternsBySwift/AbstractFactoryPattern/ShapeFactoryTwo.swift
+++ DesignPatternsBySwift/AbstractFactoryPattern/ShapeFactoryTwo.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class ShapeFactoryTwo:AbstractFactory {
+    func getShape(shape shapeType: String?) -> Shape? {
+        if let shapeType = shapeType {
+            if shapeType == "Circle" {
+                return Circle()
+            } else if shapeType == "Rectangle" {
+                return Rectangle()
+            } else if shapeType == "Square" {
+                return Square()
+            }
+        }
+        return nil
+    }
+    
+    func getColor(color: String?) -> Color? {
+        return nil
+    }
+}
